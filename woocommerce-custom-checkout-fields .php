5efdb19be7eec368ce0f7c5509913b63e3b07858
Fachremy Putra <51992373+fachremyputra@users.noreply.github.com>
2026-02-23 02:28:06 +0700

    Update woocommerce-custom-checkout-fields .php

--- woocommerce-custom-checkout-fields .php
+++ woocommerce-custom-checkout-fields .php
@@ -1,9 +1,11 @@
 <?php
 /**
  * Plugin Name: WooCommerce Custom Checkout Fields
+ * Plugin URI: https://fachremyputra.com
  * Description: Professional custom checkout field manager for WooCommerce.
  * Version: 1.0.0
  * Author: Fachremy Putra
+ * Author URI: https://fachremyputra.com
  */
 
 if (!defined('ABSPATH')) {
